ffac0a6bec1b82549eebe028fb978f24f9557db1
Luca Pasquale <lucapasquale06@gmail.com>
2023-10-04 15:28:32 +0200

    test

--- test1.py
+++ test1.py
@@ -3,12 +3,16 @@
 num1 = 5
 num2 = 20
 
+def sub(n1,n2):
+    return n1-n2
+
 def sum(n1, n2):
-    return 50
+    return n1+n2
     
 
 # Add two numbers
 sum = sum(num1, num2)
+sub=sub(num1,num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
