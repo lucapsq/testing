ae6e58474eff049de877693e6ce1d77d1aec70d6
Luca Pasquale <lucapasquale06@gmail.com>
2023-10-04 12:18:16 +0200

    added function

--- test1.py
+++ test1.py
@@ -3,8 +3,12 @@
 num1 = 5
 num2 = 10
 
+def sum(n1, n2):
+    return n1 + n2
+    
+
 # Add two numbers
-sum = num1 + num2
+sum = sum(num1, num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
